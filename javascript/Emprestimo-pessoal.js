d2d1ed950ae582834cb765eb52f66039339fe29f
Miqueias Lara <miqueias.lara@gmail.com>
2024-04-16 16:07:29 -0300

    ajusatdo botao de acessoa tela de simulador empresimo pessoal

--- javascript/Emprestimo-pessoal.js
+++ javascript/Emprestimo-pessoal.js
@@ -1,8 +1,10 @@
 const botao = document.querySelector("#botao-form");
 
+window.sr = ScrollReveal({ reset: false});
+
 let DadosCliente = {
     nome: '',
-    valorEmprestimo: 0,
+    valorEmprestimo:0 ,
     Parcelas: 0
 }
 
@@ -14,19 +16,35 @@ botao.addEventListener("click", function(event){
 
     const campo = valor.value ;
 
-    if(valor.value !== '' && DadosCliente.nome !== ''){
+    if(valor.value !== '' && DadosCliente.nome === ''){
 
         DadosCliente.nome = valor.value;
 
+        sr.reveal('.titulo-formulario', {
+            rotate: {x: 80, y: 80, z: 80,},
+            duration: 2000,
+            delay: 1000,
+           
+            
+        });
+
+        sr.reveal('.input-form', {
+            rotate: {x: 80, y: 80, z: 80,},
+            duration: 2000,
+            delay: 1000,
+           
+            
+        });
+
+        valor.value = '';
+
     }
 
-   
+    
 
     let tituloFormulario = document.getElementById("texto-titulo");
 
-    if(valor.value !== '' && DadosCliente.valorEmprestimo !== '' ){
-       
-        valor.value = '';
+    if( DadosCliente.nome !== '' && DadosCliente.valorEmprestimo == 0 ){
 
         valor.placeholder = 'R$'
 
@@ -34,25 +52,68 @@ botao.addEventListener("click", function(event){
 
         DadosCliente.valorEmprestimo = valor.value ;
 
+        sr.reveal('.titulo-formulario', {
+            rotate: {x: 0, y: 81, z: 0,},
+            delay: 500
+            
+            
+        });
+
+        sr.reveal('.input-form', {
+            rotate: {x: 0, y: 81, z: 0,},
+            delay: 500
+            
+            
+        });
+
+        valor.value = '';
+
         console.log(DadosCliente.valorEmprestimo)
     }
     
-    if(valor.value != '' && DadosCliente.Parcelas !== ''){
+    if( DadosCliente.valorEmprestimo != 0){
        
-        valor.value = '';
+        
 
         valor.placeholder = 'Ex.: 5'
 
         tituloFormulario.innerText = " Em Quantas Vezes ?"
 
-        DadosCliente.Parcelas = valor.value ;
+        
 
-        console.log(DadosCliente.Parcelas)
+        if(valor.value <=10){
+
+            DadosCliente.Parcelas = valor.value ;
+
+            sr.reveal('.titulo-formulario', {
+                rotate: {x: 0, y: 80, z: 0,},
+                duration: 2700,
+                
+                
+            });
+    
+            sr.reveal('.input-form', {
+                rotate: {x: 0, y: 80, z: 0,},
+                duration: 2700,
+                
+                
+            });
+
+            valor.value = '';
+
+             console.log(DadosCliente.Parcelas)
+        }
+        else{
+            alert('Digite um valor menor ou igua a 10 parcelas !!');
+
+            valor.value = '';
+        }
+       
     }
     
     
 
-    if(valor.value != '' ){
+    if(DadosCliente.nome !== '' && DadosCliente.valorEmprestimo != 0 && DadosCliente.Parcelas != 0  ){
 
         let result = document.getElementById("result"); 
 
@@ -62,11 +123,46 @@ botao.addEventListener("click", function(event){
         const valorEmprestimo = DadosCliente.valorEmprestimo;
         const parcelas = DadosCliente.Parcelas;
 
-        result.innerHTML = nome +'Seu Empréstimo de ' + valorEmprestimo + "ficará em" + parcelas +'x'+"R$ 500,00";
+        const taxaJuros = 0.0115 ;
+        
+        var EmprestimoComJuros = valorEmprestimo* taxaJuros ;
 
+        var ValorParcelas = (valorEmprestimo/parcelas)+EmprestimoComJuros;
 
+        var resultadoFinal = ValorParcelas*parcelas;
+
+        sr.reveal('.titulo-formulario', {
+            rotate: {x: 0, y: 80, z: 0,},
+            duration: 2700,   
+        });
+
+        sr.reveal('.input-formulario', {
+            rotate: {x: 0, y: 80, z: 0,},
+            duration: 2700,   
+        });
+
+        sr.reveal('.input-formulario', {
+            rotate: {x: 0, y: 80, z: 0,},
+            duration: 2700,   
+        });
+
+        sr.reveal('.botao', {
+            rotate: {x: 0, y: 80, z: 0,},
+            duration: 2700,   
+        });
+
+        sr.reveal('.result', {
+            rotate: {x: 0, y: 80, z: 0,},
+            duration: 2700,
+        });
+
+        result.innerHTML = nome +' Seu Empréstimo de R$ ' + valorEmprestimo + " ficará em " + parcelas +'x'+" de R$ " + Math.round(ValorParcelas) + ' no valor final de R$ ' + Math.round(resultadoFinal);
+
+       
+
+        console.log(DadosCliente)
     }
-    console.log(campo)
+    console.log(DadosCliente)
 })
     
 
